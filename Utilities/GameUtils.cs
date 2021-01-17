2488d8eb2ad034d58693129cb164020a1630ee64
DarkIrata <git@ipmix.de>
2021-01-17 13:12:30 +0100

    Missing GameUtils file

---
+++ Utilities/GameUtils.cs
@@ -0,0 +1,20 @@
+﻿using StardewModdingAPI;
+using StardewValley;
+
+namespace SVHeadlessHost.Utilities
+{
+    public static class GameUtils
+    {
+        public static void SendChatMessage(string message, IMonitor monitor = null)
+        {
+            Game1.chatBox.activate();
+            Game1.chatBox.setText(message);
+            if (monitor != null)
+            {
+                monitor.Log($"System to chat: {message}");
+            }
+
+            Game1.chatBox.chatBox.RecieveCommandInput('\r');
+        }
+    }
+}
